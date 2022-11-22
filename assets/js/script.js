8ba51ddccb3b65978cb651b61b05bfbd477a5327
phadeline <112015433+phadeline@users.noreply.github.com>
2022-11-21 21:04:47 -0500

    added for loop for videos and titles

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,11 @@
 // Get the input field
 var input = document.getElementById("myInput");
 
-
-var video = document.createElement("video");
-
 var deezer = document.getElementById("deezer");
+
+var ul = document.createElement("ul");
+
+deezer.appendChild(ul);
 
 // Execute a function when the user presses a key on the keyboard
 input.addEventListener("keypress", function (event) {
@@ -27,15 +28,16 @@ input.addEventListener("keypress", function (event) {
       .then((response) => response.json())
       .then(function (data) {
         console.log(data);
-        console.log(data.data[0].preview);
-        var ul = document.createElement("ul");
-        var li = document.createElement("li");
-        video.setAttribute("src", data.data[0].preview);
-        video.setAttribute("controls", "controls");
-        li.textContent = data.data[0].title;
-        li.appendChild(video);
-        ul.appendChild(li);
-        deezer.appendChild(ul);
+
+        for (let i = 0; i < 10; i++) {
+          var li = document.createElement("li");
+          var video = document.createElement("video");
+          video.setAttribute("src", data.data[i].preview);
+          video.setAttribute("controls", "controls");
+          li.textContent = data.data[i].title;
+          li.appendChild(video);
+          ul.appendChild(li);
+        }
       });
     event.preventDefault();
   }
@@ -43,37 +45,34 @@ input.addEventListener("keypress", function (event) {
 
 // Execute a function when the user presses a key on the keyboard
 input.addEventListener("keypress", function (event) {
-    // If the user presses the "Enter" key on the keyboard
-    var search = document.getElementById("myInput").value;
-    if (event.key === "Enter") {
-        console.log(search)
-        const options = {
-            method: 'GET',
-            headers: {
-                'X-RapidAPI-Key': 'ee310b7c05mshc4b96b46b4c303ap188ee9jsn3ab3d55f6f28',
-                'X-RapidAPI-Host': 'deezerdevs-deezer.p.rapidapi.com'
-            }
-        };
+  // If the user presses the "Enter" key on the keyboard
+  var search = document.getElementById("myInput").value;
+  if (event.key === "Enter") {
+    console.log(search);
+    const options = {
+      method: "GET",
+      headers: {
+        "X-RapidAPI-Key": "ee310b7c05mshc4b96b46b4c303ap188ee9jsn3ab3d55f6f28",
+        "X-RapidAPI-Host": "deezerdevs-deezer.p.rapidapi.com",
+      },
+    };
 
-        fetch('https://deezerdevs-deezer.p.rapidapi.com/search?q=' + search, options)
-            .then(response => response.json())
-            .then(response => console.log(response))
+    fetch(
+      "https://deezerdevs-deezer.p.rapidapi.com/search?q=" + search,
+      options
+    )
+      .then((response) => response.json())
+      .then((response) => console.log(response));
 
-    
-            // .then(function (data) {
-            //     console.log(data);
-            //   });
-            
-            // .catch(err => console.error(err));
+    // .then(function (data) {
+    //     console.log(data);
+    //   });
 
-        // Cancel the default action, if needed
-        event.preventDefault();
-        // Trigger the button element with a click
-        // document.getElementById("myBtn").click();
-        };
+    // .catch(err => console.error(err));
+
+    // Cancel the default action, if needed
+    event.preventDefault();
+    // Trigger the button element with a click
+    // document.getElementById("myBtn").click();
+  }
 });
-
-
-
-
-
