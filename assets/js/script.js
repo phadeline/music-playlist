1b0035c110e64a48d8e5e414bbdfe5b97610edfb
phadeline <112015433+phadeline@users.noreply.github.com>
2022-11-22 20:50:28 -0500

    drag and drop now works for deezer

--- assets/js/script.js
+++ assets/js/script.js
@@ -39,9 +39,9 @@ input.addEventListener("keypress", function (event) {
           video.setAttribute("src", data.data[i].preview);
           video.setAttribute("controls", "controls");
           li.textContent = data.data[i].title;
-          li.setAttribute("class","songs");
+          li.setAttribute("class", "video");
           li.appendChild(video);
-          li.setAttribute("draggable","true");
+          li.setAttribute("draggable", "true");
           ul.appendChild(li);
         }
       });
@@ -70,63 +70,34 @@ input.addEventListener("keypress", function (event) {
       options
     )
       .then((response) => response.json())
-      .then(
-        function (data) {
-          console.log(data.tracks.hits[4].track.key);
-          var key = data.tracks.hits[4].track.key;
+      .then(function (data) {
+        console.log(data.tracks.hits[4].track.key);
+        var key = data.tracks.hits[4].track.key;
 
-          fetch(
-            "https://shazam.p.rapidapi.com/songs/list-recommendations?key=" +
-              key +
-              "&locale=en-US",
-            options
-          )
-            .then((response) => response.json())
-            .then(function (data) {
-              console.log(data);
-              for (let i = 0; i < 10; i++) {
-                var li = document.createElement("li");
-                var video = document.createElement("video");
-                video.setAttribute("src", data.tracks[i].hub.actions[1].uri);
-                video.setAttribute("controls", "controls");
-                li.textContent = data.tracks[i].title;
-                li.setAttribute("class","songs");
-                li.appendChild(video);
-                li.setAttribute("draggable","true");
-                ulshazaam.appendChild(li);
-              }
-            })
-            
-        }
-        
-      );
+        fetch(
+          "https://shazam.p.rapidapi.com/songs/list-recommendations?key=" +
+            key +
+            "&locale=en-US",
+          options
+        )
+          .then((response) => response.json())
+          .then(function (data) {
+            console.log(data);
+            for (let i = 0; i < 10; i++) {
+              var li = document.createElement("li");
+              var video = document.createElement("video");
+              video.setAttribute("src", data.tracks[i].hub.actions[1].uri);
+              video.setAttribute("controls", "controls");
+              li.textContent = data.tracks[i].title;
+              li.setAttribute("class", "songs");
+              li.appendChild(video);
+              li.setAttribute("draggable", "true");
+              ulshazaam.appendChild(li);
+            }
+          });
+      });
   }
 });
-
-// // Drag and drop for Deezer
-// let dragged1 = null;
-
-// const source1 = document.getElementById("deezer");
-// source1.addEventListener("dragstart", (event) => {
-//   // store a ref. on the dragged elem
-//   dragged1 = event.target1;
-// });
-
-// const target1 = document.getElementById("playlist");
-// target1.addEventListener("dragover", (event) => {
-//   // prevent default to allow drop
-//   event.preventDefault();
-// });
-
-// target1.addEventListener("drop", (event) => {
-//   // prevent default action (open as link for some elements)
-//   event.preventDefault();
-//   // move dragged element to the selected drop target
-//   if (event.target1.className === "is-one-third") {
-//     dragged1.parentNode.removeChild(dragged1);
-//     event.target1.appendChild(dragged1);
-//   }
-// });
 
 // drag and drop for Shazaam
 let dragged = null;
@@ -144,6 +115,30 @@ target.addEventListener("dragover", (event) => {
 });
 
 target.addEventListener("drop", (event) => {
+  // prevent default action (open as link for some elements)
+  event.preventDefault();
+  // move dragged element to the selected drop target
+  if (event.target.className === "is-one-third") {
+    dragged.parentNode.removeChild(dragged);
+    event.target.appendChild(dragged);
+  }
+});
+
+//drag and drop for deezer
+
+const deezersource = document.getElementById("deezer");
+deezersource.addEventListener("dragstart", (event) => {
+  // store a ref. on the dragged elem
+  dragged = event.target;
+});
+
+const deezertarget = document.getElementById("playlist");
+deezertarget.addEventListener("dragover", (event) => {
+  // prevent default to allow drop
+  event.preventDefault();
+});
+
+deezertarget.addEventListener("drop", (event) => {
   // prevent default action (open as link for some elements)
   event.preventDefault();
   // move dragged element to the selected drop target
